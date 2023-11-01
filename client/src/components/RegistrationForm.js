24f98928d8db2940b46954440d01f465fb48b6dc
labankibet <laban.kibet@moringaschool.com>
2023-11-01 09:41:32 +0300

    form submission

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -4,6 +4,14 @@ import { Link} from 'react-router-dom';
 function RegistrationForm() {
  const [username, setUsername] = useState('');
  const [password, setPassword] = useState('');
-};
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('User Registered with username:', username);
+    history.push('/login');
+ };
+
+
+}
 
 export default RegistrationForm;
