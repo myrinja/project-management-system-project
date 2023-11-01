fa5b7704539c86003a8666e1ff6a60e90feea47a
labankibet <laban.kibet@moringaschool.com>
2023-11-01 09:39:17 +0300

    creating components

---
+++ client/src/components/RegistrationForm.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Link} from 'react-router-dom';
+
+function RegistrationForm() {
+ const [username, setUsername] = useState('');
+ const [password, setPassword] = useState('');
+};
+
+export default RegistrationForm;
