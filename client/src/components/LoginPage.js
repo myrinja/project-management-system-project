29aad936884cc85f89776b42c2a38ab0b42d12d2
labankibet <laban.kibet@moringaschool.com>
2023-11-02 21:30:21 +0300

    registration/login

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -5,10 +5,36 @@ function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Handle user login here.
+    // Log in the user with the backend API.
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    // Check if the login was successful.
+    if (response.status === 200) {
+      // Show an alert to the user that they were logged in successfully.
+      alert('You were logged in successfully!');
+
+      // Store the user's login information in local storage.
+      localStorage.setItem('isLoggedIn', true);
+      localStorage.setItem('username', username);
+
+      // Redirect the user to the home page.
+      window.location.href = '/';
+    } else {
+      // Show an alert to the user that their login failed.
+      alert('There was an error logging you in. Please try again later.');
+    }
   };
 
   return (
@@ -21,7 +47,7 @@ function LoginPage() {
       </form>
       <p>Don't have an account? <Link to="/register">Register</Link></p>
     </div>
- );
+  );
 }
 
 export default LoginPage;
