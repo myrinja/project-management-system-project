a66453a8ba5630ff351c64849fdcc2b0f42939c7
Njokimary <marynjoki738@gmail.com>
2023-11-05 23:48:06 +0300

    feat:user authentication

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,34 +1,70 @@
 import React, { useState } from 'react';
-import './LoginForm.css';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './LoginForm.css'
 
 const LoginForm = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: ''
-  });
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(formData);
+    fetch('http://127.0.0.1:5000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          navigate('/posts'); // Redirect to the posts page
+        } else {
+          setError('Incorrect email or password. Please try again.');
+        }
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" value={formData.password} onChange={handleChange} />
-      </label>
-      <button type="submit">Login</button>
-    </form>
+    <div className="login-container">
+      <h2 className="login-title">Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="form-group">
+          <label htmlFor="email" className="label">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="input"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="label">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+          />
+        </div>
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+      {error && <p className="error">{error}</p>}
+      <p>
+        Don't have an account? <Link to="/register" className="registration-link">Register here</Link>
+      </p>
+    </div>
   );
 };
 
