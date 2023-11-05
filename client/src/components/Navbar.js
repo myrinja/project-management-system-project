8f33a5af36263a59be180d76372c7c75bfb679b1
Njokimary <marynjoki738@gmail.com>
2023-11-05 23:50:10 +0300

    feat:updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,17 +7,9 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
         <Link className="navbar-brand" to="/">PostHaven</Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        
           <span className="navbar-toggler-icon"></span>
-        </button>
+        
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             
